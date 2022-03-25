d9cc1b080503c107da0a8d3835e14833a0067d6f
Alex Prior <47067268+priorpeak@users.noreply.github.com>
2022-03-25 13:46:41 -0400

    Parsed noise data

--- noiseHub/analyze_data.py
+++ noiseHub/analyze_data.py
@@ -18,16 +18,27 @@ client = boto3.client(
 door_sql_query = 'SELECT * FROM "{DATABASE_NAME}"."door_table" WHERE time > ago(24h) ORDER BY time'.format(DATABASE_NAME=DATABASE_NAME)
 noise_sql_query = 'SELECT * FROM "{DATABASE_NAME}"."noise_data" WHERE time > ago(24h) ORDER BY time'.format(DATABASE_NAME=DATABASE_NAME)
 
-door_data = client.query(
-    QueryString=door_sql_query
-    # MaxRows=10
-)
+# door_data = client.query(
+#     QueryString=door_sql_query
+#     # MaxRows=10
+# )
 
 noise_data = client.query(
     QueryString=noise_sql_query
     # MaxRows=10
 )
 
-print(door_data)
-print('\n')
-print(noise_data)
+# print(door_data)
+# print('\n')
+# print(noise_data)
+
+# Noise data parsing
+noise_data = noise_data['Rows']
+for row in noise_data:
+    # print(row['Data'])
+
+    timestamp = row['Data'][2]['ScalarValue']
+    noise_level = row['Data'][3]['ScalarValue']
+
+    print(f'timestamp: {timestamp}')
+    print(f'noise_level: {noise_level}')
