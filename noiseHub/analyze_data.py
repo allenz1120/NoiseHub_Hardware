3793ca5f2a2b0a5e59a0cb7b361fb13a6634bcdb
bbrewer1 <bbrewer1@bu.edu>
2022-04-14 17:03:34 -0400

    Updated parsing for new tables
    Co-authored-by: Ibrahim Chand <ibchand@users.noreply.github.com>

--- noiseHub/analyze_data.py
+++ noiseHub/analyze_data.py
@@ -20,7 +20,7 @@ client = boto3.client(
 
 # Queries data from last 24h
 door_sql_query = 'SELECT * FROM "{DATABASE_NAME}"."door_table" WHERE time > ago(24h) ORDER BY time'.format(DATABASE_NAME=DATABASE_NAME)
-noise_sql_query = 'SELECT * FROM "{DATABASE_NAME}"."noise_data" WHERE time > ago(24h) ORDER BY time'.format(DATABASE_NAME=DATABASE_NAME)
+noise_temp_sql_query = 'SELECT * FROM "{DATABASE_NAME}"."noise_temp_table" WHERE time > ago(24h) ORDER BY time'.format(DATABASE_NAME=DATABASE_NAME)
 
 # # Queries all data (Not just last 24h)
 # door_sql_query = 'SELECT * FROM "{DATABASE_NAME}"."door_table" ORDER BY time'.format(DATABASE_NAME=DATABASE_NAME)
@@ -31,10 +31,12 @@ door_data = client.query(
     # MaxRows=10
 )
 
-noise_data = client.query(
-    QueryString=noise_sql_query
+noise_temp_data = client.query(
+    QueryString=noise_temp_sql_query
     # MaxRows=10
 )
+
+#print(noise_data)
 
 time_placeholder = datetime.now()
 
@@ -70,20 +72,31 @@ dynamo_data = {
 # print(noise_data)
 
 # Noise data parsing
-noise_data = noise_data['Rows']
+noise_temp_data = noise_temp_data['Rows']
 
+# Initialize high noise level variables
 curr_loud_duration = 0
 curr_loud_timestamp = time_placeholder
 max_loud_duration = 0
 max_loud_time = time_placeholder
 
+# Initialize low noise level variables
 curr_quiet_duration = 0
 curr_quiet_timestamp = time_placeholder
 max_quiet_duration = 0
 max_quiet_time = time_placeholder
 
-for row in noise_data:
-    timestamp = row['Data'][2]['ScalarValue'][:-3]
+# Initialize max and min temps
+max_temp = 0.0
+max_temp_time = time_placeholder
+
+min_temp = 100.0
+min_temp_time = time_placeholder
+
+for row in noise_temp_data:
+
+    # Retrieve timestamp
+    timestamp = row['Data'][3]['ScalarValue'][:-3]
 
     # Convert timestamp string to DateTime object
     datetime_object = datetime.strptime(timestamp, '%Y-%m-%d %H:%M:%S.%f')
@@ -92,10 +105,30 @@ for row in noise_data:
     dynamo_data['noise_timestamp'].append(str(datetime_object))
     
     # Isolate the noise level of the data
-    noise_level = row['Data'][0]['ScalarValue']
+    noise_level = row['Data'][1]['ScalarValue']
+
+    # Isolate the temp level of the data
+    temp = row['Data'][0]['ScalarValue']
+    temp = round(float(temp) * 1.8 + 32, 2)
 
     # Append noise level to global dictionary
     dynamo_data['noise_data'].append(int(noise_level))
+
+    # Append temp to global dictionary
+    dynamo_data['temp_data'].append(str(temp))
+
+    # Print statements to verify noise levels and temperatures
+    print("Time: " + str(datetime_object) + "\n")
+    print("Noise Level: " + str(noise_level) + "\n")
+    print("Temperature: " + str(temp) + "\n\n")
+
+
+    if(temp > max_temp):
+        max_temp = temp
+        max_temp_time = datetime_object
+    elif (temp < min_temp):
+        min_temp = temp
+        min_temp_time = datetime_object
 
     # If the noise level is loud and previous reading was not loud, increment loud duration and save the current timestamp
     if noise_level == '2' and curr_loud_duration == 0:
@@ -138,11 +171,24 @@ for row in noise_data:
     # print(f'max_quiet_duration: {max_quiet_duration}')
     # print(f'max_quiet_time: {max_quiet_time}')
 
+print("Max Temp: " + str(max_temp) + "\t\tTime: " + str(max_temp_time) + "\n")
+print("Min Temp: " + str(min_temp) + "\t\tTime: " + str(min_temp_time) + "\n")
+print("Max Loud Duration: " + str(max_loud_duration) + "\t\tTime: " + str(max_loud_duration) + "\n")
+print("Max Quiet Duration: " + str(max_quiet_duration) + "\t\tTime: " + str(max_quiet_duration) + "\n")
+
+
+
+
 # Append max and min variables to global dictionary
 dynamo_data['max_loud_timestamp'] = str(max_loud_time)
 dynamo_data['max_loud_value'] = max_loud_duration
 dynamo_data['max_quiet_timestamp'] = str(max_quiet_time)
 dynamo_data['max_quiet_value'] = max_quiet_duration
+
+dynamo_data['max_temp_timestamp'] = str(max_temp_time)
+dynamo_data['max_temp_value'] = str(max_temp)
+dynamo_data['min_temp_timestamp'] = str(min_temp_time)
+dynamo_data['min_temp_value'] = str(min_temp)
 
 # print(dynamo_data)
 
@@ -150,17 +196,15 @@ dynamo_data['max_quiet_value'] = max_quiet_duration
 # Door data parsing
 door_data = door_data['Rows']
 
+#change temps to query from new table
+
 max_heads = 0
 max_heads_time = time_placeholder
 
 min_heads = 100
 min_heads_time = time_placeholder
 
-max_temp = 0.0
-max_temp_time = time_placeholder
-
-min_temp = 100.0
-min_temp_time = time_placeholder
+#keep head stuff 
 
 for row in door_data:
     # print(row['Data'])
@@ -172,26 +216,12 @@ for row in door_data:
     
     # Append timestamp to global dictionary
     dynamo_data['head_timestamp'].append(str(datetime_object))
-    dynamo_data['temp_timestamp'].append(str(datetime_object))
     
-    temp = row['Data'][1]['ScalarValue']
-    temp = round(float(temp) * 1.8 + 32, 2)
-
-    # Append temp(erature) to global dictionary
-    dynamo_data['temp_data'].append(str(temp))
-
     heads = int(row['Data'][0]['ScalarValue'])
     
     # Append headcount to global dictionary
     dynamo_data['head_data'].append(heads)
-    
-    if(temp > max_temp):
-        max_temp = temp
-        max_temp_time = datetime_object
-    elif (temp < min_temp):
-        min_temp = temp
-        min_temp_time = datetime_object
-        
+       
     if(heads > max_heads):
         max_heads = heads
         max_heads_time = datetime_object
@@ -208,11 +238,6 @@ dynamo_data['max_head_timestamp'] = str(max_heads_time)
 dynamo_data['max_head_value'] = max_heads
 dynamo_data['min_head_timestamp'] = str(min_heads_time)
 dynamo_data['min_head_value'] = min_heads
-
-dynamo_data['max_temp_timestamp'] = str(max_temp_time)
-dynamo_data['max_temp_value'] = str(max_temp)
-dynamo_data['min_temp_timestamp'] = str(min_temp_time)
-dynamo_data['min_temp_value'] = str(min_temp)
 
 # print(dynamo_data)
 
@@ -242,4 +267,5 @@ response = table.update_item(
     },
     ReturnValues="UPDATED_NEW"
 )
-print(response)
+##print(response)
+#
